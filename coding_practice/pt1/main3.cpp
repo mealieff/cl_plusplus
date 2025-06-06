a1664b655c8d39dad98354361f7b3296419f9cea
mealieff <91396479+mealieff@users.noreply.github.com>
2025-06-06 08:49:38 -0400

    updated pt1, main3

---
+++ coding_practice/pt1/main3.cpp
@@ -0,0 +1,2 @@
+// For this practice we will work with primitive and derived data types
+// prompt: You're a bouncer. Add a function bool isAdult(int age) that returns true if age ≥ 21 and call it from main.
